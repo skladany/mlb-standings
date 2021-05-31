2a87564b942350126b53af09914d31f58732fd3f
Steve Skladany <steve@skladany.net>
2021-05-31 06:01:36 -0400

    Add ability to retrieve cached data

--- functions/standings/standings.js
+++ functions/standings/standings.js
@@ -3,7 +3,27 @@ const fs = require("fs");
 
 const API_ENDPOINT = "https://erikberg.com/mlb/standings.json";
 
-exports.handler = async () => {
+exports.handler = async (req) => {
+  const { date: req_date } = req.queryStringParameters;
+  console.log(`REQ ${req_date}`);
+
+  // Look for cached copy of data
+  if (req_date != undefined) {
+    try {
+      console.log(`Returning cached data for ${req_date}`);
+      const data = fs.readFileSync(`./data/${req_date}.json`, "utf8");
+      const json = JSON.parse(data);
+      json.cached = true;
+      return {
+        statusCode: 200,
+        body: JSON.stringify(json),
+      };
+    } catch (err) {
+      console.log(`${req_date} has not been cached.`);
+      console.error(err);
+    }
+  }
+
   let response, data, full_date, standings;
   try {
     response = await fetch(API_ENDPOINT);
@@ -42,7 +62,7 @@ exports.handler = async () => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ full_date, standings }),
+      body: JSON.stringify({ full_date, standings, cached: false }),
     };
   } catch (err) {
     return {
