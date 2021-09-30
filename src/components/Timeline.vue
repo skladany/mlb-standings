89d1e9eff13b7b73cd8b1f31c1e26547c998385b
Steve Skladany <steve@skladany.net>
2021-09-30 09:22:01 -0400

    Timeline of last week's data

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
+    <!-- <div style="width:100vw;height:80vh"> -->
     <LineChart v-if="loaded" :chartdata="chartdata" :options="options" />
+    <!-- </div> -->
   </div>
 </template>
 
@@ -9,9 +11,26 @@ import LineChart from "./LineChart.vue";
 const { players } = require("../data/players.json");
 
 const ENDPOINT = "/.netlify/functions";
-// const START_DATE = "2021-04-01";
-const START_DATE = "2021-08-01";
-const END_DATE = "2021-09-08";
+
+const date = new Date();
+const season_end = new Date("2021-10-04");
+let startDate, endDate;
+
+if (date > season_end) {
+  startDate = "2021-09-23";
+  endDate = "2021-09-30";
+} else {
+  endDate = date.toISOString().split("T")[0];
+
+  // Last week
+  date.setDate(date.getDate() - 7);
+  startDate = date.toISOString().split("T")[0];
+}
+
+console.log({ startDate });
+console.log({ endDate });
+console.log({ season_end });
+
 import { fetchTimeline } from "../api/api";
 import { getDates } from "../utils/utils";
 
@@ -26,12 +45,12 @@ export default {
   async mounted() {
     this.loaded = false;
     try {
-      let labels = getDates({ startDate: START_DATE, endDate: END_DATE });
+      let labels = getDates({ startDate: startDate, endDate: endDate });
 
       let datasets = [];
       players.forEach(async (player) => {
         const standings = await fetchTimeline(
-          `${ENDPOINT}/timeline?player=${player.id}&startDate=${START_DATE}&endDate=${END_DATE}`
+          `${ENDPOINT}/timeline?player=${player.id}&startDate=${startDate}&endDate=${endDate}`
         );
 
         console.log({ standings });
